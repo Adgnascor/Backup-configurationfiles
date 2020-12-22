b265cf45ada3ca9ad158a2758e9fb0ac4a619ed2
Adgnascor <william.l.dev@gmail.com>
2020-12-22 18:51:34 +0100

    Initial flow of application

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO.Abstractions;
+using System.Linq;
 using Application.src;
 
 namespace Application
@@ -8,7 +10,30 @@ namespace Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome!");
+            var fileSystem = new FileSystem();
+            var initBackup = new InitializeBackup(fileSystem, null);
+
+            if(!initBackup.FolderExists())
+                 initBackup.CreateFolder();
+
+            var paths = fileSystem.Directory.GetFiles(Environment.CurrentDirectory);
+
+
+            for(var i = 0; i< paths.Length; i++)
+                Console.WriteLine($"{i}. {paths[i]}");
+
+            Console.Write("Backup: ");
+            var fileIndex = Console.ReadLine();
+
+            Console.Write("Name of application: ");
+            var folderName = Console.ReadLine();
+            var file = fileSystem.Path.GetFileName(paths[int.Parse(fileIndex)]);
+            var backUp = new Backup(new ApplicationFolder(file , paths[int.Parse(fileIndex)] , folderName), fileSystem, initBackup);
+
+            backUp.CreateApplicationFolder();
+            backUp.CopyFile();
+            backUp.SaveOriginalFilePath();
+
             Console.ReadKey();
         }
     }
